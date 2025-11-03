8bf1815adc409f7611f4e395f6be4e18d6cf01cb
Piotr Witczak <p.witczak@issi.uz.zgora.pl>
2025-11-03 14:10:10 +0000

    added unit tests and some automation

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
-from distutils.core import setup
+from setuptools import setup
 from Cython.Build import cythonize
 import numpy
 
-
 setup(
-    name = "tropicalpy",
+    name="tropicalpy",
     version='0.0.1b',
     description='Python Cython-based tropical geometry',
     author='Jeremy Lane, Piotr Witczak',
     author_email='pkwitczak@gmail.com',
-    ext_modules = cythonize("tropicalpy.pyx"),
-    include_dirs=[numpy.get_include()]
+    ext_modules=cythonize("tropicalpy.pyx"),
+    include_dirs=[numpy.get_include()],
 )
