f652f1953504ebb58b8d6725da9c6245071e3139
Piotr Witczak <p.witczak@issi.uz.zgora.pl>
2025-11-04 10:36:02 +0000

    fixed workflow

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -4,9 +4,6 @@ import numpy as np
 import sys
 import os
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-import pyximport
-pyximport.install()
 import tropicalpy
 
 class TestTropicalPy(unittest.TestCase):
